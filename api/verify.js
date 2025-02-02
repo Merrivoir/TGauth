dc14c07ac87418c5110a349df916f23a4cd3e407
Paul Lvov <48524294+Merrivoir@users.noreply.github.com>
2025-02-02 07:20:00 +0500

    Update verify.js

--- api/verify.js
+++ api/verify.js
@@ -7,11 +7,13 @@ app.post('/verify', (req, res) => {
     const token = req.headers.authorization?.split(' ')[1];
     
     if (!token) {
+        console.log(`Токен в запросе не найден`)
         return res.status(401).json({ valid: false });
     }
 
     try {
         const decoded = jwt.verify(token, process.env.SECRET_KEY);
+        console.log(`decode: ${decoded}`)
         
         // Дополнительная проверка в базе данных
         checkUserInDatabase(decoded.id).then(exists => {
